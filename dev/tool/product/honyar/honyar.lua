3fbd8ebdb3339c5e51e1f7b974da65e4a41ff175
fei <feiguoyou@hotmail.com>
2016-03-02 09:34:14 +0800

    完全忽略.svn目录, 信息由dev.svn.info.txt指示

--- dev/tool/product/honyar/honyar.lua
+++ dev/tool/product/honyar/honyar.lua
@@ -33,40 +33,74 @@ function LOGTBL(tblObj)
 end
 
 --[[ MUST
-	1. json <-> bin
-	2. json <-> json
+	0. UART json <-> bin
+	1. PIPE/IPC json <-> json
 ]]
-function getCvtType()
-	local whatType = 1
-	-- baud - dataBits parity(None:0, Odd:1, Even:2) stopBits
-	local baud = '"9600-8N1"'
-	local str = string.format('{"whatType":%d,"baud":%s}', whatType, baud)
-
+function s1GetVer()
+	-- body
+	local str = '1.0'
 	return string.len(str), str
 end
 
 --[[ MUST
-	查询设备状态。
-
-	每个设备都约定需要一条或者多条指令可以获取到设备的所有状态。
-	返回的状态由固件来缓存。
-	指令必须为完整的指令，可以直接对电控板操作的。
-	在用户定义产品时填充。
-
-	标准接口，该方法由云端自动生成。
+	whatCvtType:
+	0. UART json <-> bin
+	1. PIPE/IPC json <-> json
 ]]
-function getQueries()
-	local query = string.char( 0xbb, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfa, 0x44 )
-	local queryCountLen = string.char( 0x09, 0x00 )
+function s1GetCvtType()
 
-	return string.len( queryCountLen ), queryCountLen, string.len( query ), query
+	local whatCvtType = 0
+	-- delay time (ms) for the interval during write & read. 
+	local delay = 5
+	--[[
+		E.g."9600-8N1"
+		FORMAT [baud(9600, ...) - dataBits(8, 9, ...) parity(None:0, Odd:1, Even:2) stopBits(1, 2)]
+		refer to func(halIO.c)
+		void *halUartOpen(int baud, int dataBits, int stopBits, int parity, int flowCtrl);
+	]] 
+	local baud = '"9600-8N1"'
+	local str = string.format('{"whatCvtType":%d,"baud":%s}', whatCvtType, baud)
+
+	return string.len(str), str, delay
 end
 
-function getQueries()
+--[[ MUST
+	查询设备状态。
+	每个设备都约定需要一条或者多条指令可以获取到设备的所有状态。
+]]
+function s1getQueries()
 	local query = string.char( 0xa5, 0xa5, 0x5a, 0x5a, 0xb1, 0xc0, 0x01, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00 )
 	local queryCountLen = string.char( 0x0d, 0x00 )
 
 	return string.len( queryCountLen ), queryCountLen, string.len( query ), query
+end
+
+--[[ MUST
+	WIFI 重置命令判别
+]]
+function s1GetValidKind(data)
+    local reset = string.char( 0xA5, 0xA5, 0x5A, 0x5A, 0x98, 0xC1, 0xE8, 0x03, 0x00, 0x00, 0x00, 0x00 )
+	-- print (data, #data)
+
+	--[[ MUST
+		wifi reset cmd
+	]]
+	if reset == data then
+		-- print '1'
+		return 1
+	end
+
+	--[[ MUST
+		valid LOW LEVEL status cmd
+	]]
+	if 9 == #data then
+		-- print '2'
+		return 2
+	end
+
+	-- print '0'
+	-- invalid kind
+	return 0
 end
 
 -- 插排
@@ -74,7 +108,7 @@ end
 -- a5 a5 5a 5a b9 c0 04 00 04 00 00 00 00 04    # 返回 04 00
 -- a5 a5 5a 5a c1 c0 02 00 03 00 00 0f 00       # 控制 02 00
 -- a5 a5 5a 5a c8 c0 04 00 04 00 00 00 0f 04    # 返回 04 00
-function cvtStd2Pri(json)
+function s1CvtStd2Pri(json)
 	local tb = cjson.decode(json)
 	local ctrl = tb["ctrl"]
 	local cmdTbl = { 0xa5, 0xa5, 0x5a, 0x5a, 0x00, 0x00, 0x02, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00 }
@@ -106,8 +140,9 @@ function cvtStd2Pri(json)
 end
 
 --[[ MUST
+	return value: return 0, 0, if the input param 'bin' is not valid
 ]]
-function cvtPri2Std(bin)
+function s1CvtPri2Std(bin)
     local dataTbl = {}
     local str = '{"idx1":%d,"idx2":%d,"idx3":%d,"idx4":%d}'
     -- print (#bin)
@@ -117,8 +152,7 @@ function cvtPri2Std(bin)
     if dataTbl[1] == 0xa5 and dataTbl[2] == 0xa5 and dataTbl[3] == 0x5a and dataTbl[4] == 0x5a then
         str = string.format(str, (dataTbl[13] >> 0) & 0x1, (dataTbl[13] >> 1) & 0x1, (dataTbl[13] >> 2) & 0x1, (dataTbl[13] >> 3) & 0x1)
     else
-        str = string.format(str, -1, -1, -1, -1)
+        str = ""
     end
-    print (str)
     return string.len(str), str
 end
---
+++ dev/tool/product/honyar/packGenS1.sh
@@ -0,0 +1,1 @@
+python ../../packit.py --genS1 ./0x1c3000.bin ./honyar.lua 0x4000 0x5000
