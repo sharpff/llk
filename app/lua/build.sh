96e9f854d757a8b23186c443fc665f12a50d4114
fei <feiguoyou@hotmail.com>
2016-05-20 16:44:25 +0800

    lua编译脚本修改

--- app/lua/build.sh
+++ app/lua/build.sh
@@ -55,13 +55,13 @@ touch "$MAIN_PATH/sw/data.c"
 $MAIN_PATH/tool/SubWCRev $MAIN_PATH $MAIN_PATH/tool/version.template.h $MAIN_PATH/sw/version.h
 
 pushd $PATH_SENGINE > /dev/null 2>&1
-make
+make PLATFORM="linux" $*
 popd > /dev/null 2>&1
 
 pushd $PATH_LELINK > /dev/null 2>&1
-make
+make PLATFORM="linux" $*
 popd > /dev/null 2>&1
 
-make clean && make
+make PLATFORM="linux" $*
 echo done
 
