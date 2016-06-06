bbb61fae7d7d28d622d8b4e98026e20829d72960
fei <feiguoyou@hotmail.com>
2016-06-06 15:22:29 +0800

    honyar脚本设置默认路由

--- tool/product/honyar/genProfile.sh
+++ tool/product/honyar/genProfile.sh
@@ -14,8 +14,8 @@ $MAIN_PATH/lelinkTool.py \
 
 $MAIN_PATH/lelinkTool.py \
     --genpriv \
-    --ssid Letv_lelink \
-    --passwd 987654321 \
+    --ssid ff \
+    --passwd fengfeng2qiqi \
     -o ./0x1c8000.bin
 
 $MAIN_PATH/lelinkTool.py \
