1756ad2068ee2f0c1f3bb6f9ac592281f7898735
fei <feiguoyou@hotmail.com>
2016-07-13 09:45:32 +0800

    添加netConfig说明

--- app/linux_x86/sdkpack.sub.sh
+++ app/linux_x86/sdkpack.sub.sh
@@ -9,7 +9,8 @@
 echo $(pwd)
 PACK_FILES=$PACK_FILES:"debug.mak"
 PACK_FILES=$PACK_FILES:"Makefile"
-PACK_FILES=$PACK_FILES:"main.c"
+PACK_FILES=$PACK_FILES:"*.c"
+PACK_FILES=$PACK_FILES:"*.h"
 PACK_FILES=$PACK_FILES:"/lib/lelink/Debug-$PLATFORM/*.a@lib/lelink/Debug-$PLATFORM"
 PACK_FILES=$PACK_FILES:"/lib/sengine/Debug-$PLATFORM/*.a@lib/sengine/Debug-$PLATFORM/"
 
