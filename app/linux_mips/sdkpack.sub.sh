5e5581e9f5abe3f42aac0e66207dbafd68ce8d91
Fred <linfeng@le.com>
2016-07-19 21:05:41 +0800

    [PACKAGE] for qca9531 mips-linux-openwrt

--- app/linux_mips/sdkpack.sub.sh
+++ app/linux_mips/sdkpack.sub.sh
@@ -10,8 +10,7 @@ echo $(pwd)
 PACK_FILES=$PACK_FILES:"debug.mak"
 PACK_FILES=$PACK_FILES:"Makefile"
 PACK_FILES=$PACK_FILES:"*.c"
-PACK_FILES=$PACK_FILES:"*.h"
+# PACK_FILES=$PACK_FILES:"*.h"
 PACK_FILES=$PACK_FILES:"/lib/Debug-$PLATFORM/*.a@lib/Debug-$PLATFORM"
 
-./build.sh
-
+./build.sh MYXPATH=/home/lf/dev/compiler/OpenWrt-Toolchain-ar71xx-for-mips_r2-gcc-4.6-linaro_uClibc-0.9.33.2/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/ MYXPREFIX=mips-openwrt-linux-
