5c9abdde5ba74be192c269963957e27f3f9e9447
Fred <linfeng@le.com>
2016-12-07 21:01:16 +0800

    processing in wifi config timeout

--- app/linux/main.c
+++ app/linux/main.c
@@ -245,14 +245,40 @@ void thread_input_check(void *arg) {
 
 #define PORT_ONLY_FOR_VM 0 // (NW_SELF_PORT + 100) // the port for r2r should be 0, 
 
-#define DO_AIR_CONFIG    
-#define ENABLE_WIFI_SOFT_AP 1
+// #define DO_AIR_CONFIG    
+// #define ENABLE_WIFI_SOFT_AP 1
+
+// #define TIMEOUT_BEGIN_SEC(ss, go1st) {\
+//     static uint32_t tsStart;\
+//     static uint32_t tsEnd;\
+//     static uint8_t tmpGo1st = go1st; \
+//     if (!tsStart) { \
+//         tsStart = tsEnd = halGetTimeStamp(); \
+//     } \
+//     tsEnd = halGetTimeStamp(); \
+//     APPLOG("TIMEOUT_BEGIN_SEC ss[%d] start[%d] end[%d]", ss, tsStart, tsEnd); \
+//     if (tmpGo1st || ss <= (tsEnd - tsStart)) { \
+//         tsStart = tsEnd; tmpGo1st = 0;
+
+// #define TIMEOUT_ELSE } else {
+
+// #define TIMEOUT_END }}
 
 #ifndef DO_AIR_CONFIG
 int main(int argc, char *argv[]) {
     pthread_t id;
     int i, ret = 0;
     AuthData authData;
+
+
+    // do {
+    //     TIMEOUT_BEGIN_SEC(3, 0)
+    //         APPLOG("IF");
+    //     TIMEOUT_ELSE
+    //         APPLOG("ELSE");
+    //     TIMEOUT_END
+    //     halDelayms(500);
+    // } while (1);
 
     // {
     //     // char buf[1024] = {0};
