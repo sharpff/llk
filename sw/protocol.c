dbb8a3a56389a6cd9ec049f8e9512577d6106790
Fred <linfeng@le.com>
2016-12-14 17:20:05 +0800

    sdev mngmnt(local)

--- sw/protocol.c
+++ sw/protocol.c
@@ -1428,6 +1428,10 @@ static int cbDiscoverStatusChangedLocalReq(void *ctx, const CmdHeaderInfo* cmdIn
         ret = getSDevStatus(cmdInfo->reserved-1, status, sizeof(status));
     }
 
+    if (sengineHasDevs()) {
+        senginePollingRules(status, sizeof(status));
+    }
+
     LELOG("NO need token[%d][%s]", ret, status);
     // test only
     // static int mm = 0;
